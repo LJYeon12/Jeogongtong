8904cbb7a794c0e821692d4ccc38bd8e96b16dc9
suyong choi <dpfprtus@gmail.com>
2024-02-17 23:40:07 +0900

    [FEAT] Navigator Bar

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:jeogongtong_front/constants/colors.dart';
 
-class BottomNavigator extends StatelessWidget {
+class BottomNavigator extends StatefulWidget {
   const BottomNavigator({super.key});
+
+  @override
+  State<BottomNavigator> createState() => _BottomNavigatorState();
+}
+
+class _BottomNavigatorState extends State<BottomNavigator> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+        currentIndex: _currentIndex,
         onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
           switch (index) {
             case 0:
               Navigator.pushNamed(context, '/home');
@@ -20,7 +32,7 @@ class BottomNavigator extends StatelessWidget {
               break;
           }
         },
-        items: [
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
               icon: SvgPicture.asset("assets/images/home_icon.svg"),
               label: "Home"),
